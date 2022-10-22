50df750157be482ddabdbae0de3846a530313401
Santiago Patino <spatino@arkusnexus.com>
2022-10-22 17:33:36 -0500

    🎉  Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = (): JSX.Element => {
+  return (
+    <div className='App'>
+      PokeTeam
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <head>
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' rel='stylesheet' />
+    </head>
+    <App />
+  </React.StrictMode>
+)
