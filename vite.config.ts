f4890d139d736a1fdbd79f7c3e8077278cdc50f4
Santiago Patino <spatino@arkusnexus.com>
2022-10-23 20:56:24 -0500

    ⚙️ Add PostCSS Mixins

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import postcssNesting from "postcss-nested"
+import postcssMixins from "postcss-mixins"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
-      plugins: [postcssNesting],
+      plugins: [postcssNesting, postcssMixins],
     },
   },
 })
