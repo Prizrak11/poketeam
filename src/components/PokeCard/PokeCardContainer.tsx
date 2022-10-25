8492d96e563466f5e99bc0625d9ef0e771f35075
Santiago Patino <spatino@arkusnexus.com>
2022-10-24 20:14:29 -0500

    💄 Support dual type pokemon in bg color

--- src/components/PokeCard/PokeCardContainer.tsx
+++ src/components/PokeCard/PokeCardContainer.tsx
@@ -11,14 +11,18 @@ export interface CardContainerProps {
 }
 
 const PokeCardContainer: FC<CardContainerProps> = ({ pokemon, remove, className, children }): JSX.Element => {
-  const typeColor = pokemonTypes.get(pokemon.types[0]).color
+  const typeColor = pokemon.types.length === 1
+    ? pokemonTypes.get(pokemon.types[0]).color
+    : `linear-gradient(45deg,  
+          ${pokemonTypes.get(pokemon.types[0]).color} 50%,
+          ${pokemonTypes.get(pokemon.types[1]).color} 100%)`
 
   className = className ?? ''
 
   return (
     <div className={`${className}`}>
       {(remove != null) && <div onClick={() => remove(pokemon)} className={styles.removeBtn}>X</div>}
-      <div style={{ backgroundColor: typeColor }} className={styles.background} />
+      <div style={{ background: typeColor }} className={styles.background} />
       {children}
     </div>
   )
