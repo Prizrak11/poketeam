c3b4107e75e79a21f20e7d9995ebbc6af5a0d151
Santiago Patino <spatino@arkusnexus.com>
2022-11-01 17:51:56 -0500

    ✨ Show power, accuracy and priority of moves

--- src/components/MoveCard/MoveCard.tsx
+++ src/components/MoveCard/MoveCard.tsx
@@ -1,6 +1,9 @@
 import TypeBadge from 'components/TypeBadge/TypeBadge'
 import useAttacker from 'hooks/useAttacker'
 import { FC } from 'react'
+import { FaFistRaised } from 'react-icons/fa'
+import { FiTarget } from 'react-icons/fi'
+import { TbArrowBigUpLine } from 'react-icons/tb'
 import ReactTooltip from 'react-tooltip'
 import { PokemonMove } from 'types/moves'
 import { Pokemon } from 'types/pokemon'
@@ -19,7 +22,7 @@ const MoveCard: FC<MoveCardProps> = ({ move, open = false }): JSX.Element => {
 
   const getTip = (type: PokemonType, attacker?: Pokemon, to?: number): string => {
     if (open && (to == null || to === 1)) return ''
-    if (move.stab > 1 && to === null) return `${type.name} has STAB`
+    if (move.stab > 1 && attacker == null) return `${type.name} has STAB`
     if (attacker == null || to == null || to === 1) return type.name
     if (to === 0) return `${type.name} doesn't affect ${attacker?.name} `
     if (move.stab > 1) return `${type.name} has STAB and hits x${to} to ${attacker?.name}`
@@ -34,11 +37,38 @@ const MoveCard: FC<MoveCardProps> = ({ move, open = false }): JSX.Element => {
 
   return (
     <section className={sectionClass}>
+      <div className={styles.background} />
       <ReactTooltip className={styles.tooltip} />
-      <div data-tip={getTip(move.type, attacker, weak)}>
+      <div className={styles.badge} data-tip={getTip(move.type, attacker, weak)}>
         <TypeBadge type={move.type} weak={{ to: weak * move.stab }} big={open} stab={Boolean(move.stab > 1)} />
       </div>
       <p className={styles.title}>{move.name}</p>
+      <section className={styles.info}>
+        {
+          move.power != null && (
+            <div data-tip='Power' className={styles.value}>
+              <FaFistRaised />
+              <p>{move.power}</p>
+            </div>
+          )
+        }
+        {
+          move.accuracy != null && (
+            <div data-tip='Accuracy' className={styles.value}>
+              <FiTarget />
+              <p>{move.accuracy}</p>
+            </div>
+          )
+        }
+        {
+          move.priority != null && move.priority !== 0 && (
+            <div data-tip='Priority' className={styles.value}>
+              <TbArrowBigUpLine />
+              <p>{move.priority}</p>
+            </div>
+          )
+        }
+      </section>
     </section>
   )
 }
