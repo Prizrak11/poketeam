a9a4bcbf3e80759287086d5c3e7b27625db26edc
Santiago Patino <santiagopatino@drata.com>
2023-11-22 20:24:50 -0500

    ✏️ Fix file naming typos

--- src/components/Attacker/Attacker.tsx
+++ src/components/Attacker/Attacker.tsx
@@ -4,7 +4,7 @@ import Spinner from 'components/Spinner/Spinner'
 import useAttacker from 'hooks/useAttacker'
 import { usePokemonSearchModal } from 'hooks/modals'
 import { FC } from 'react'
-import styles from './Atacker.module.css'
+import styles from './Attacker.module.css'
 import { MenuItem } from 'components/Tooltip/TooltipMenu'
 
 const Attacker: FC = (): JSX.Element => {
